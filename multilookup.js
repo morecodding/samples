0408e644072a6f733a42cd5d9b22b9cf887eebe6
Lucas Marques <lmarquesmoreira@users.noreply.github.com>
2016-03-04 18:18:35 -0300

    Update multilookup.js

--- multilookup.js
+++ multilookup.js
@@ -3,72 +3,72 @@
     window.CSRManager =  window.CSRManager || {};
 
     window.CSRManager.Parser = {
-    parseObjToHtml : function (html, obj){
-        var sub = function(o){
-            var c = o.indexOf(":");
-            if (c == -1)
-                return undefined;
-            return { id: o.substr(0, c), v: o.substr(c+1) }
-        }
-        for(var i =0, l = obj.length; i < l; i++){
-            var v = sub(obj[i]);
-            if(v != undefined)
-                html = html.replace("{{"+v.id+"}}", v.v);
-        }
-        return html;
-    },
-    parseMultiLookupValue : function(cs, toreplace){
-        var m ="";
-        for(var i = 0; i < cs.length; i++){
-            if (m == undefined || m == "") {
-                m = cs[i].id.replace(toreplace, "") + ";#" + cs[i].value;
+        parseObjToHtml : function (html, obj){
+            var sub = function(o){
+                var c = o.indexOf(":");
+                if (c == -1)
+                    return undefined;
+                return { id: o.substr(0, c), v: o.substr(c+1) }
             }
-            else {
-                m = m + ";#" + cs[i].id.replace(toreplace, "") + ";#" + cs[i].value;
-            }        
+            for(var i =0, l = obj.length; i < l; i++){
+                var v = sub(obj[i]);
+                if(v != undefined)
+                    html = html.replace("{{"+v.id+"}}", v.v);
+            }
+            return html;
+        },
+        parseMultiLookupValue : function(cs, toreplace){
+            var m ="";
+            for(var i = 0; i < cs.length; i++){
+                if (m == undefined || m == "") {
+                    m = cs[i].id.replace(toreplace, "") + ";#" + cs[i].value;
+                }
+                else {
+                    m = m + ";#" + cs[i].id.replace(toreplace, "") + ";#" + cs[i].value;
+                }        
+            }
+            return m;
         }
-        return m;
-    }
     }
 
     window.CSRManager.Elements = {
-     createCheckboxes : function(choices, name){
-        var box = "";
-        for(var i = 0, l = choices.length; i < l; i++){
-            var c = this.createCheckbox(choices[i], name);
-            box += c.label;
-            box += c.checkbox;
+        createCheckboxes : function(choices, name){
+            var box = "";
+            for(var i = 0, l = choices.length; i < l; i++){
+                var c = this.createCheckbox(choices[i], name);
+                box += c.label;
+                box += c.checkbox;
+            }
+            return box;
+        },
+        createCheckbox : function(choice, name){
+            var checkbox =  "<input id=\"{{id}}\" type=\"checkbox\" name=\"{{name}}\" value=\"{{value}}\" />";
+            var label = "<label for=\"{{id}}\">{{value}}</label>";
+
+            var id = "id_" + name  + choice.LookupId;
+            checkbox = window.CSRManager.Parser.parseObjToHtml(checkbox, ["id:"+id, "name:" +name, "value:" + choice.LookupValue]);
+            label = window.CSRManager.Parser.parseObjToHtml(label, [ "id:" + id, "value:" + choice.LookupValue] );
+
+            return { checkbox: checkbox, label : label, value: label + checkbox };
         }
-        return box;
-    },
-    createCheckbox : function(choice, name){
-        var checkbox =  "<input id=\"{{id}}\" type=\"checkbox\" name=\"{{name}}\" value=\"{{value}}\" />";
-        var label = "<label for=\"{{id}}\">{{value}}</label>";
-
-        var id = "id_" + name  + choice.LookupId;
-        checkbox = window.CSRManager.Parser.parseObjToHtml(checkbox, ["id:"+id, "name:" +name, "value:" + choice.LookupValue]);
-        label = window.CSRManager.Parser.parseObjToHtml(label, [ "id:" + id, "value:" + choice.LookupValue] );
-
-        return { checkbox: checkbox, label : label, value: label + checkbox };
-    }
     }
 
     window.CSRManager.Render = {
-    checkbox: function(ctx){
-        var choices = ctx.CurrentFieldSchema.Choices; //choices array
+        checkbox: function(ctx){
+            var choices = ctx.CurrentFieldSchema.Choices; //choices array
 
-        var div = "<div id=\"{{id}}\">{{box}}</div>";
+            var div = "<div id=\"{{id}}\">{{box}}</div>";
 
-        var boxes = window.CSRManager.Elements.createCheckboxes(choices, "produto");
-        div = window.CSRManager.Parser.parseObjToHtml(div, ["id:produto_box", "box:"+boxes]);
+            var boxes = window.CSRManager.Elements.createCheckboxes(choices, "produto");
+            div = window.CSRManager.Parser.parseObjToHtml(div, ["id:produto_box", "box:"+boxes]);
 
-        SPClientTemplates.Utility.GetFormContextForCurrentField(ctx).registerGetValueCallback(ctx.CurrentFieldSchema.Name, function(){
-            var cs = document.querySelectorAll("#produto_box input[type='checkbox']:checked");
-            return window.CSRManager.Parser.parseMultiLookupValue(cs, "id_produto");
-        });
+            SPClientTemplates.Utility.GetFormContextForCurrentField(ctx).registerGetValueCallback(ctx.CurrentFieldSchema.Name, function(){
+                var cs = document.querySelectorAll("#produto_box input[type='checkbox']:checked");
+                return window.CSRManager.Parser.parseMultiLookupValue(cs, "id_produto");
+            });
 
-        return div;
-    }    
+            return div;
+        }    
     }    
 
 })();
